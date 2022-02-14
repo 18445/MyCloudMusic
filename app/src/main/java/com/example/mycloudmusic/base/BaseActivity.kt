e809741feb403b25aa0c353a8f89fa3ede0358e9
Cysan <53166358+18445@users.noreply.github.com>
2022-02-15 04:51:47 +0800

    顶部栏透明

--- app/src/main/java/com/example/mycloudmusic/base/BaseActivity.kt
+++ app/src/main/java/com/example/mycloudmusic/base/BaseActivity.kt
@@ -11,6 +11,13 @@ import android.content.Intent
 
 import android.content.BroadcastReceiver
 import android.content.IntentFilter
+import android.graphics.Color
+import android.view.View
+import android.view.ViewGroup
+import android.view.Window
+import android.view.WindowManager
+import androidx.core.view.ViewCompat
+import android.os.Build
 
 
 
@@ -31,9 +38,20 @@ open class BaseActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
         supportActionBar?.hide();
+
+        //顶部透明
+        val window = window
+        window.clearFlags(
+            WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+                    or WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION
+        )
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE)
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = Color.TRANSPARENT
+        window.navigationBarColor = Color.TRANSPARENT
 
         //将Activity 中的Window 的背景图设置为空,解决Android Activity切换时出现白屏问题
 //        window.setBackgroundDrawable(null)
@@ -50,6 +68,8 @@ open class BaseActivity: AppCompatActivity() {
             }
         }
 
+
+
         //打印当前活动的activity
         Log.d(TAG, javaClass.simpleName) // 知晓当前是在哪一个活动
 
@@ -60,7 +80,6 @@ open class BaseActivity: AppCompatActivity() {
         screenDendity = displayMetrics.density.toInt() //屏幕密度比例
         screenDendityDpi = displayMetrics.densityDpi //屏幕密度
     }
-
 
 
 }
